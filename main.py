c39ff4aec8a40bc0c06ecf860c684daa521e6060
Aksel488 <aksel.susegg@gmail.com>
2022-10-13 14:04:03 +0200

    download dataset and show mesh

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import numpy as np
+import os
+import trimesh
 from matplotlib import pyplot as plt
-from utils import read_off
+from utils import read_off, show_mesh, download_modelnet10
+
+import tensorflow as tf
+from tensorflow import keras
 
 
-path = 'data/ModelNet10/bathtub/train/bathtub_0001.off'
+# Download dataset if not exists
+path_here = os.path.dirname(__file__)
+DATA_DIR = os.path.join(path_here, 'datasets/ModelNet10')
+if not os.path.isdir(DATA_DIR):
+    print("Downloading")
+    DATA_DIR = download_modelnet10()
 
-vertices_bath, faces_bath = read_off(path)
-x = [x[0] for x in vertices_bath]
-y = [y[1] for y in vertices_bath]
-z = [z[2] for z in vertices_bath]
+path = 'bathtub/train/bathtub_0001.off'
+show_mesh(os.path.join(DATA_DIR, path))
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# # ax.scatter(x, y, z, c='r', marker='o')
-# ax.contourf(X=x, Y=y, Z=z)
-# plt.show()
 
 
